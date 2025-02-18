40751d3b9e87935c7b7289b4a879bc2c346cadf0
VasaBhanuPrakash <21eg107a62@gmail.com>
2025-02-18 21:23:08 +0530

    Uploaded 16-02-2025

--- Daily Assignments/15-02-2025/Spring-project-with-jdbc/src/main/java/org/bhanu/EmpRunner.java
+++ Daily Assignments/15-02-2025/Spring-project-with-jdbc/src/main/java/org/bhanu/EmpRunner.java
@@ -1,16 +1,55 @@
 package org.bhanu;
+import java.util.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class EmpRunner {
 	public static void main(String arg[]) {
 		ApplicationContext a=new ClassPathXmlApplicationContext("config.xml");
 		EmpDao ed=(EmpDao)a.getBean("edao");
-		int status=ed.saveEmployee(new Empdetails(2,"Bhanu",56000));
-		System.out.println("Row inserted successfully "+status);
-		status=ed.updateEmployee(new Empdetails(1,"Srinivasulu",76000));
-		System.out.println("Row updated successfully "+status);
-		status=ed.deleteEmployee(new Empdetails(2));
-		System.out.println("Row deleted successfully "+status);
-		((ClassPathXmlApplicationContext) a).close();
+		Scanner sc=new Scanner(System.in);
+		do {
+			System.out.println("1.Insert\n2.Update\n3.Delete\n4.Exit");
+			System.out.print("Enter your choice: ");
+			int status=0;
+			switch(sc.nextInt()) {
+			case 1:{
+				try{
+					status=ed.saveEmployee(new Empdetails(2,"Bhanu",6055));
+					System.out.println("Rows inserted successfully "+status);
+				}
+				catch(Exception e) {
+					System.out.println("A person  with that ID already exists");
+				}
+				finally {
+					break;
+				}
+			}
+			case 2:{
+				status=ed.updateEmployee(new Empdetails(4,"Gopi",6564));
+				if(status>0) {
+					System.out.println("Rows updated successfully "+status);
+				}
+				else {
+					System.out.println("Row doesn't exist");
+				}
+				break;
+			}
+			case 3:{
+				status=ed.deleteEmployee(new Empdetails(2));
+				if(status>0) {
+					System.out.println("Rows deleted successfully "+status);
+				}
+				else {
+					System.out.println("Row doesn't exist with id 2");
+				}
+				break;
+			}
+			case 4:{
+				return;
+			}
+			
+			}
+		}while(true);
+		
 	}
 }
